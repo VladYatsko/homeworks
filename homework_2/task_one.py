08f027c429865d30252577c6161d3a6d348cc603
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2022-11-21 21:25:06 +0100

    Updated placeholders for input() text

--- homework_2/task_one.py
+++ homework_2/task_one.py
@@ -1,9 +1,9 @@
 import math
 
 point_x = int(input("Please specify x coordinate: "))
-point_y = int(input("Please specify x coordinate: "))
-point_x_1 = int(input("Please specify x coordinate: "))
-point_y_1 = int(input("Please specify x coordinate: "))
+point_y = int(input("Please specify y coordinate: "))
+point_x_1 = int(input("Please specify x1 coordinate: "))
+point_y_1 = int(input("Please specify y1 coordinate: "))
 distance = round(math.sqrt((point_x_1 - point_x)**2 +
                            (point_y_1 - point_y)**2), 2)
 
