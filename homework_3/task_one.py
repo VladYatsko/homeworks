e61b05bf20c0addfd0fcb0a71a366218ae4c197d
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2022-11-24 23:32:29 +0100

    Hometask 3 done. Updated requirements to have only package names.

---
+++ homework_3/task_one.py
@@ -0,0 +1,3 @@
+user_input = input("Please specify your string: ")
+formatted_string = user_input.replace('#', '/')
+print(formatted_string)
---
+++ homework_3/task_two.py
@@ -0,0 +1,4 @@
+user_name = input("Please specify user name with two words: ")
+splitted_name = list(user_name.split(" "))
+reversed_name = f'{splitted_name[1]} {splitted_name[0]}'
+print(reversed_name)
