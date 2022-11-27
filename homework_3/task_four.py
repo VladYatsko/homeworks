75f27d07354a4a518e4e270b65d0891add7d68d4
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2022-11-27 13:31:17 +0100

    Updated Tasks two and three. Reworked task four to be more flexible.

--- homework_3/task_four.py
+++ homework_3/task_four.py
@@ -1,16 +1,17 @@
 first_dict = {'a': 1, 'b': 2, 'c': 3}
 second_dict = {'c': 3, 'd': 4, 'e': 5}
-letters_array = ['a', 'b', 'c', 'd', 'e']
+merged_dict = first_dict | second_dict
+array_of_keys = merged_dict.keys()
 array_of_united_values = []
 counter = 0
 
-for letter in letters_array:
+for key in merged_dict:
     array_of_united_values\
-        .append([first_dict.get(letter), second_dict.get(letter)])
+        .append([first_dict.get(key), second_dict.get(key)])
 
 first_dict.update(second_dict)
-for letter in letters_array:
-    first_dict[letter] = list(array_of_united_values[counter])
+for key in first_dict:
+    first_dict[key] = array_of_united_values[counter]
     counter += 1
 
 print(first_dict)
