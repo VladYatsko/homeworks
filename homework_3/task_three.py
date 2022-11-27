ff57f2f4c4edb60340063b36a4c06ed05a9254d8
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2022-11-27 13:31:17 +0100

    Updated Tasks two and three. Reworked task four to be more flexible.

--- homework_3/task_three.py
+++ homework_3/task_three.py
@@ -1,11 +1,6 @@
 import random
 
-random_list = []
-counter = 0
-while counter < 10:
-    random_list.append(random.randrange(-100, 100))
-    counter += 1
-
+random_list = [random.randrange(-100, 100) for element in range(10)]
 print(f'firstly initialized list is {random_list}')
 random_list[2] = random.randint(-100, 100)
 del random_list[6]
