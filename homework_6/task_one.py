dcd7fb71ca443fec69fef670d4de0f2df8eb2f97
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2022-12-07 22:37:08 +0100

    Fixed calculating formula

--- homework_6/task_one.py
+++ homework_6/task_one.py
@@ -3,12 +3,21 @@ import math
 
 def distance(*args: tuple):
     list_of_values = []
+    list_of_distances = []
+    index = 0
     for point_one, point_two in args:
         one_point_value = (point_two - point_one)**2
         list_of_values.append(one_point_value)
+    while index < len(list_of_values) - 1:
+        if index > len(list_of_values):
+            break
+        list_of_distances.append(math.sqrt(list_of_values[index]
+                                           + list_of_values[index + 1]))
+        index += 1
+
     return f'Distance between {len(args)} points is' \
-           f' {round(math.sqrt(sum(list_of_values)), 2)}'
+           f' {round(sum(list_of_distances), 2)}'
 
 
-result = distance((1, 3), (3, 5), (5, 7))
+result = distance((3, 5), (5, 7), (7, 9))
 print(result)
