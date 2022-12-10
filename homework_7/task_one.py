30ea76fa9e4a97339b06ab85e6af8fdf22cbc068
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2022-12-10 19:32:18 +0100

    Adding hometask 7

---
+++ homework_7/task_one.py
@@ -0,0 +1,23 @@
+def get_winners(results: list):
+    maximal_result = 0
+    winner_numbers = []
+    for points in results:
+        if points > maximal_result:
+            maximal_result = points
+    winner_numbers.append(results.index(maximal_result)+1)
+    results[results.index(maximal_result)] = 0
+    maximal_result = 0
+    for points in results:
+        if points > maximal_result:
+            maximal_result = points
+    winner_numbers.append(results.index(maximal_result) + 1)
+    results[results.index(maximal_result)] = 0
+    maximal_result = 0
+    for points in results:
+        if points > maximal_result:
+            maximal_result = points
+    winner_numbers.append(results.index(maximal_result) + 1)
+    return winner_numbers
+
+
+print(get_winners([100, 10, 20, 30, 40, 5, 80, 21]))
