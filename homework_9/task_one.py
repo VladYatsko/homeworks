adb41bd8cc43252af6c892a59132a18bcf35d1c2
Uladzislau Yatsko <yatsko.vladislav@gmail.com>
2022-12-21 11:01:38 +0100

    Adding hometask 9

---
+++ homework_9/task_one.py
@@ -0,0 +1,31 @@
+class Triangle:
+    def __init__(self, side_a, side_b, side_c):
+        if Triangle._is_exists(side_a, side_b, side_c):
+            self.side_a = side_a
+            self.side_b = side_b
+            self.side_c = side_c
+
+    @staticmethod
+    def _is_exists(side_a, side_b, side_c):
+        if side_a + side_b < side_c or \
+                side_a + side_c < side_b \
+                or side_b + side_c < side_a:
+            raise ValueError('no such triangle exists')
+        else:
+            return True
+
+    def is_right_angled(self):
+        return self.side_a ** 2 + self.side_b ** 2 == self.side_c ** 2
+
+    def _perimeter(self):
+        return self.side_a + self.side_b + self.side_c
+
+    def __eq__(self, other):
+        return self._perimeter() == other._perimeter()
+
+
+tr1 = Triangle(3, 4, 5)
+print(tr1.is_right_angled())
+tr2 = Triangle(3, 4, 6)
+print(tr2.is_right_angled())
+print(tr1.__eq__(tr2))
